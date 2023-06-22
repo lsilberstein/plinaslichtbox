210019fbee089b4504751448759f84d827d6aa96
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-22 16:41:34 +0200

    wrote script to find HUE Bridge in you network

--- pi/inits.js
+++ pi/inits.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import ApiAxios from "./ApiAxios.js";
 
-const init = () => {
-    console.log("initializing information...");
-    console.log("searching for Philips Hue Bridge...");
-    axios.get("https://discovery.meethue.com/", (err, res, body) => {
+const init = async () => {
+    console.log("Searching for HUE Bridge...");
+    const res = await ApiAxios.get("https://discovery.meethue.com/", (err, res, body) => {
         if (err) {
             console.log("error:", err);
+            return err;
         } else {
-            console.log("body:", body);
+            return res;
         }
     });
-    console.log("initialization complete.");
+    console.log("I found a HUE Bridge connected at this IP address:", res.data[0].internalipaddress);
 }
 
 export default init;
