94fa66976da896e0ebcd17335a6558c801afc887
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 21:49:03 +0200

    was ist denn hier los?

--- pi/lights.js
+++ pi/lights.js
@@ -1,4 +1,5 @@
 const PHILIPS_HUE_API_ID = process.env.PHILIPS_HUE_API_ID;
+const PHILIPS_API_LIGHT_ID = process.env.PHILIPS_API_LIGHT_ID;
 let lightsAxios;
 
 export function setLightsAxios(axios) {
@@ -6,16 +7,19 @@ export function setLightsAxios(axios) {
 }
 
 export async function turnLightOn() {
-    lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/1/state`, { on: true, bri: 100 })
-        .then(res => {
-            // not working yet. Needs fix!
-            console.log("I turned the light on:");
+    console.log("PHILIPS_HUE_API_ID:", PHILIPS_HUE_API_ID);
+    console.log("PHILIPS_API_LIGHT_ID:", PHILIPS_API_LIGHT_ID);
+    lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_API_LIGHT_ID}/state`, { on: true, bri: 100 })
+        .then((err, res) => {
+            if(err) console.log("error:", err);
+            else {console.log("I turned the light on!", res);}
+            
         })
 }
 
 export async function turnLightOff() {
-    lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/1/state`, { on: false })
+    lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_API_LIGHT_ID}/state`, { on: false })
         .then(res => {
-            console.log("I turned the light off:");
+            console.log("I turned the light off!");
         })
 }
