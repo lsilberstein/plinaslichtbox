316267c4b4c80023a5d023f46d1aac2171a0c1b4
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 22:19:36 +0200

    added route

--- pi/lights.js
+++ pi/lights.js
@@ -8,7 +8,7 @@ export async function turnLightOn() {
     const PHILIPS_HUE_API_ID = process.env.PHILIPS_HUE_API_ID;
     const PHILIPS_HUE_API_LIGHT_ID = process.env.PHILIPS_HUE_API_LIGHT_ID;
 
-    const res = await lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_HUE_API_LIGHT_ID}/state`, { on: true, bri: 100 });
+    const res = await lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_HUE_API_LIGHT_ID}/state`, { on: true, bri: 50 });
 
     console.log("I turned the light on!", res.data);
 }
--- pi/setup/routes.js
+++ pi/setup/routes.js
@@ -1,9 +1,16 @@
 import app from "../app.js";
+import { switchState } from "../states.js";
 
 const initRoutes = () => {
     app.get("/test", (req, res) => {
-    res.send("Welcome the PlinasLichtBox API!")
+        res.send("Welcome the PlinasLichtBox API!")
     });
+
+    app.put("/thinkingOfYou", (req, res) => {
+        switchState("api");
+        console.log("Someone was thinking of you!");
+        res.send("Thanks for thinking of me!");
+    })
 }
 
 export default initRoutes;
