27fbe7c47e2d0d4bc69ea983e08839377a57a576
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 23:00:28 +0200

    fix

--- pi/requests.js
+++ pi/requests.js
@@ -4,7 +4,7 @@ export async function sendLightRequest () {
     const EXTERNAL_DOMAIN = process.env.EXTERNAL_DOMAIN;
     const EXTERNAL_PORT = process.env.EXTERNAL_PORT;
 
-    const axios = createAxios(`http://${EXTERNAL_DOMAIN}.duckdns.org:${EXTERNAL_PORT}/thinkingOfYou`);
+    const axios = createAxios(`http://${EXTERNAL_DOMAIN}.duckdns.org:${EXTERNAL_PORT}`);
 
     try {
         res = await axios.put("/thinkingOfYou");
