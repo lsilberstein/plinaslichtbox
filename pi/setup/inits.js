db2bc57914458c2a001582e7fa9c9ab09c0bc367
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 18:50:54 +0200

    add call to find lights connected to HUE Bridge

--- pi/setup/inits.js
+++ pi/setup/inits.js
@@ -2,9 +2,9 @@ import ApiAxios from "../ApiAxios.js";
 import { initButton } from "./button.js";
 import { duckDNSinterval } from "./intervals.js";
 
-const init = async () => {
-    console.log("Searching for HUE Bridge...");
-    
+const init = async () => { 
+    const PHILIPS_HUE_API_ID = process.env.PHILIPS_HUE_API_ID;
+
     const res = await ApiAxios.get("https://discovery.meethue.com/", (err, res, body) => {
         if (err) {
             console.log("error:", err);
@@ -14,6 +14,17 @@ const init = async () => {
         }
     });
     console.log("I found a HUE Bridge connected at this IP address:", res.data[0].internalipaddress);
+
+    const res2 = await ApiAxios.get(`${res.data[0].internalipaddress}/api/${PHILIPS_HUE_API_ID}/lights`, (err, res, body) => {
+        if (err) {
+            console.log("error:", err);
+            return err;
+        } else {
+            return res;
+        }
+    });
+
+    console.log("I found these lights connected to the HUE Bridge:", res2.data);
 
     initButton();
 
