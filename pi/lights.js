3f134fa3b7bd50f1a9901a60b30736a6124efbb7
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 19:30:24 +0200

    add comment

--- pi/lights.js
+++ pi/lights.js
@@ -8,6 +8,7 @@ export function setLightsAxios(axios) {
 export async function turnLightOn() {
     lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/1/state`, { on: true, bri: 100 })
         .then(res => {
+            // not working yet. Needs fix!
             console.log("I turned the light on:");
         })
 }
