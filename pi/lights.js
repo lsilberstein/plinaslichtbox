19b5d9994b8f838a4d03a8797844e4a68fd4f972
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 21:51:35 +0200

    sdf

--- pi/lights.js
+++ pi/lights.js
@@ -11,8 +11,8 @@ export async function turnLightOn() {
     console.log("PHILIPS_API_LIGHT_ID:", PHILIPS_API_LIGHT_ID);
     lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_API_LIGHT_ID}/state`, { on: true, bri: 100 })
         .then((err, res) => {
-            if(err) console.log("error:", err);
-            else {console.log("I turned the light on!", res);}
+            if(err) console.log("error:");
+            else {console.log("I turned the light on!");}
             
         })
 }
