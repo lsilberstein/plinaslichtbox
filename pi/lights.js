74e858aad0132e2880b60c2360d13cf6ec5b7203
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 22:14:25 +0200

    stupid mistake

--- pi/lights.js
+++ pi/lights.js
@@ -17,7 +17,7 @@ export async function turnLightOff() {
     const PHILIPS_HUE_API_ID = process.env.PHILIPS_HUE_API_ID;
     const PHILIPS_HUE_API_LIGHT_ID = process.env.PHILIPS_HUE_API_LIGHT_ID;
 
-    const res = await lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_HUE_API_LIGHT_ID}/state`, { on: true, bri: 100 });
+    const res = await lightsAxios.put(`api/${PHILIPS_HUE_API_ID}/lights/${PHILIPS_HUE_API_LIGHT_ID}/state`, { on: false });
 
     console.log("I turned the light off!", res.data);
 }
