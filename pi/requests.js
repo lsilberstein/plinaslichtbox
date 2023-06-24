2ed91a0d9dda01f3abd3b40923a1f19b0af2f98b
Linus Silberstein <l.silberstein@ostfalia.de>
2023-06-24 22:43:48 +0200

    made function async

--- pi/requests.js
+++ pi/requests.js
@@ -1,16 +1,15 @@
 import createAxios from "./ApiAxios.js";
 
-export const sendLightRequest = () => {
+export async function sendLightRequest () {
     const EXTERNAL_DOMAIN = process.env.EXTERNAL_DOMAIN;
     const EXTERNAL_PORT = process.env.EXTERNAL_PORT;
 
     const axios = createAxios(`http://${EXTERNAL_DOMAIN}.duckdns.org:${EXTERNAL_PORT}/thinkingOfYou`);
 
     try {
-        res = axios.put("/thinkingOfYou");
+        res = await axios.put("/thinkingOfYou");
+        console.log("I have sent a note!", res);
     } catch (err) {
         console.log("Error sending note", err);
     }
-
-    console.log("I have sent a note!", res);
 }
